f71ad3a90cff0897ef0eacc42b60df93aaf8f46f
lucas <dantaslucas337@gmail.com>
2022-10-11 18:13:44 -0300

    Exercicio 3

---
+++ src/Exerci3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Exerci3 {
+
+public static void main(String args[]) {
+            
+    int a,b;
+
+    Scanner sc = new Scanner(System.in); 
+    
+    System.out.println("Insira os valores para a soma: ");
+    a = sc.nextInt();
+    b = sc.nextInt();
+    
+    System.out.println("Resultado da soma: "+(a+b));
+   
+     
+}
+}

6d930c77af14f220d9e21b8eb1284ac5aa500d21
lucas <dantaslucas337@gmail.com>
2022-10-11 18:22:25 -0300

    Exercicio 3 parte 2

--- src/Exerci3.java
+++ src/Exerci3.java
@@ -3,7 +3,10 @@ import java.util.Scanner;
 public class Exerci3 {
 
 public static void main(String args[]) {
-            
+    Scanner sc = new Scanner(System.in); 
+    /*    
+
+    exercicio 3 
     int a,b;
 
     Scanner sc = new Scanner(System.in); 
@@ -12,8 +15,16 @@ public static void main(String args[]) {
     a = sc.nextInt();
     b = sc.nextInt();
     
-    System.out.println("Resultado da soma: "+(a+b));
+    System.out.println("Resultado da soma: "+(a+b));*/
    
-     
+    //area = pi*raio2 
+
+
+    System.out.println("Insira o tamanho do raio do circulo: ");
+    double raio,area;
+    raio = sc.nextDouble(); 
+    area = 3.14159*(raio*raio);
+    
+    System.out.printf("A area do circulo é de: %.4f",area);
 }
 }

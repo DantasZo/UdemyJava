f90e374af986bc2685f94dce01716ef1412d4ffb
lucas <dantaslucas337@gmail.com>
2022-10-13 12:59:01 -0300

    Exercicio 3 parte 5 concluida

--- src/Exerci3.java
+++ src/Exerci3.java
@@ -43,7 +43,7 @@ public static void main(String args[]) {
 
 }*/
 
-
+/*
 Locale.setDefault(Locale.US);
 System.out.println("Insira o Numero do colaborador,  numero de horas trabalhadas e o salario hora, respectivamente.");
 
@@ -55,6 +55,29 @@ double salario = sc.nextDouble()*horas;
 
 System.out.printf("Numero do colaborador: %d %n Salario: %.2f", funcionario, salario);
 
+ */
+/* 
+Locale.setDefault(Locale.US);
+
+		int cod1, cod2, qte1, qte2;
+	    double preco1, preco2, total;
+
+	    cod1 = sc.nextInt();
+	    qte1 = sc.nextInt();
+	    preco1 = sc.nextDouble();
+	    
+	    cod2 = sc.nextInt();
+	    qte2 = sc.nextInt();
+	    preco2 = sc.nextDouble();
+
+	    total = preco1 * qte1 + preco2 * qte2;
+
+	    System.out.printf("VALOR A PAGAR: R$ %.2f%n", total);
+		
+		sc.close();
+
+
+*/
 
 }
 }

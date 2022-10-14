ef813f7c021a83e24c8a376ef3aae4b38fde0e0b
lucas <dantaslucas337@gmail.com>
2022-10-14 15:47:48 -0300

    exercicio 5 parte 2

--- src/Exerci5.java
+++ src/Exerci5.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class Exerci5 {
     
     public static void main(String[] args){
-
         Scanner sc = new Scanner(System.in);
+     /* Scanner sc = new Scanner(System.in);
 
         int x = sc.nextInt();
 
@@ -12,6 +12,14 @@ public class Exerci5 {
         System.out.println("Numero positivo!");
         else 
         System.out.println("Numero negativo!");
+        */
+        int x = sc.nextInt();
+        if (x % 2 == 0)
+        System.out.println("Numero par! ");
+        else
+        System.out.println("numero impar!");
+
+
 
     }
 }
